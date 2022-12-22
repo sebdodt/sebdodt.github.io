81f4abfa30edb41c66e090d297a3392a6fceccde
sebdodt <mats.dodt@googlemail.com>
2022-12-22 02:00:52 +0100

    changed template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,86 +1,56 @@
-const nav = document.querySelector(".header"),
-  hamBtn = document.querySelector(".ham__btn"),
-  navMenu = document.querySelector(".nav__menu"),
-  navLinks = document.querySelectorAll(".nav__link"),
-  sections = document.querySelectorAll("section[id]"),
-  themeBtn = document.querySelector(".theme__toggle");
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
-  // Hamburger menu start
-  hamBtn.addEventListener("click", (e) => {
-    navMenu.classList.toggle("show__menu");
-  });
-  
-  navLinks.forEach((e) => {
-    e.addEventListener("click", () => navMenu.classList.remove("show__menu"));
-  });
-  // Hamburger menu start
-
-
-// Current section view in nav links 
-const scrollActive = () => {
-  const scrollY = window.scrollY;
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute("id");
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      navMenu
-        .querySelector("a[href*=" + sectionId + "]")
-        .classList.add("active__link");
-    } else {
-      navMenu
-        .querySelector("a[href*=" + sectionId + "]")
-        .classList.remove("active__link");
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
     }
-  });
-};
-window.addEventListener("scroll", scrollActive);
-
-
-// Shadow add to header after scroll 
-const scrollHeader = () => {
-  if (window.scrollY >= 80) nav.classList.add("scroll__header");
-  else nav.classList.remove("scroll__header");
-};
-window.addEventListener("scroll", scrollHeader);
-
-
-// Scroll to top button 
-function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener("scroll", scrollUp);
+showMenu('nav-toggle','nav-menu')
 
-// Theme Changer
-// Theme Changer
-// Theme Changer
-let theme = "light";
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
-const activeDark = (event) => {
-  event.classList.remove("bx-toggle-left");
-  event.classList.add("bx-toggle-right");
-  document.querySelector("body").classList.add("dark__theme");
-};
-
-const deActiveDark = (event) => {
-  event.classList.remove("bx-toggle-right");
-  event.classList.add("bx-toggle-left");
-  document.querySelector("body").classList.remove("dark__theme");
-};
-
-if (localStorage.getItem("theme__rdw") === null) {
-  localStorage.setItem("theme__rdw", "light");
-} else {
-  theme = localStorage.getItem("theme__rdw");
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
 }
-theme === "dark" ? activeDark(themeBtn) : null;
-themeBtn.addEventListener("click", (e) => {
-  theme = theme === "dark" ? "light" : "dark";
-  localStorage.setItem("theme__rdw", theme);
-  theme === "dark" ? activeDark(e.target) : deActiveDark(e.target);
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
 });
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
